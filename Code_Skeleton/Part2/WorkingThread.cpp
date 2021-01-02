8a70db00ae36ec173b139c015453318a9882cde6
Mor Paz <1912.mor@gmail.com>
2021-01-02 19:16:29 +0200

    Add small test file Update rounding logic

--- Code_Skeleton/Part2/WorkingThread.cpp
+++ Code_Skeleton/Part2/WorkingThread.cpp
@@ -13,7 +13,7 @@ WorkingThread::WorkingThread(uint thread_id, field_mat *current, field_mat *next
 cellNeighbors WorkingThread::calculate_neighbors(field_mat *field, int i, int j, int height, int width) {
     cellNeighbors env;
     env.numAlive = 0;
-    for (int k = 0; k < 7; ++k) {
+    for (int k = 0; k < 8; ++k) {
         env.neighborConc.push_back(0);
     }
     for (int k = i - 1; k <= i + 1; ++k) {
@@ -23,7 +23,7 @@ cellNeighbors WorkingThread::calculate_neighbors(field_mat *field, int i, int j,
             }
             if (is_legal_neighbor(k, l, height, width) && (*field)[k][l] != 0) {
                 env.numAlive++;
-                env.neighborConc[(*field)[k][l] % 7]++;
+                env.neighborConc[(*field)[k][l]]++;
             }
         }
     }
@@ -36,8 +36,6 @@ int WorkingThread::find_dominant_species(cellNeighbors env) {
     int maxIndex = -1;
     int maxVal = 0;
 
-    // 1 is red
-    // 0 is black (it's actually 7)
     for (int i = 1; i < env.neighborConc.size(); ++i) {
         int tempVal = env.neighborConc[i] * i;
         if (tempVal > maxVal) {
@@ -46,10 +44,6 @@ int WorkingThread::find_dominant_species(cellNeighbors env) {
         }
     }
 
-    int lastVal = env.neighborConc[0] * 7;
-    if (lastVal > maxVal) {
-        maxIndex = 7;
-    }
     return maxIndex;
 }
 
@@ -62,12 +56,8 @@ int WorkingThread::change_species_from_neighbors(cellNeighbors env, int selfVal)
             counter += env.neighborConc[i];
         }
     }
-
-    if (env.neighborConc[0] != 0) {
-        sum += env.neighborConc[0] * 7;
-        counter += env.neighborConc[0];
-    }
-    return std::round(sum / counter);
+    double res = (double)sum/counter;
+    return std::round(res);
 }
 
 bool WorkingThread::is_legal_neighbor(int i, int j, int height, int width) {
